9908dfc08b4386ac245e5854bd75743fc00e87c8
imaximciuc <maximciuc.iurii@gmail.com>
2020-04-09 22:29:02 +0300

    Add auth components

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import {LayoutRouting} from "./layout.routing";
 import {LayoutComponent} from "./layout.component";
 
+import {SignInComponent} from "./auth/sign-in/sign-in.component";
+import {SignUpComponent} from "./auth/sign-up/sign-up.component";
+
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
--- src/app/layout/layout.routing.ts
+++ src/app/layout/layout.routing.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LayoutComponent} from "./layout.component";
+import {SignInComponent} from "./auth/sign-in/sign-in.component";
+import {SignUpComponent} from "./auth/sign-up/sign-up.component";
 
 const routes: Routes = [
   {path: '', component: LayoutComponent, children: [
-    {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)}
+    {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
+    {path: 'sign-in', component: SignInComponent },
+    {path: 'sign-up', component: SignUpComponent }
 
     ]
   }
