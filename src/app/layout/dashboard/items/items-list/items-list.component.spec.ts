d821f96eb0e4bc5ce80b78a0caed6937360b00f3
imaximciuc <maximciuc.iurii@gmail.com>
2020-05-12 17:04:06 +0300

    Update tests, add itemHelper

--- src/app/layout/dashboard/items/items-list/items-list.component.spec.ts
+++ src/app/layout/dashboard/items/items-list/items-list.component.spec.ts
@@ -1,19 +1,25 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ItemsListComponent} from './items-list.component';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ItemService} from "../../../../shared/services/item.service";
 import {Component, CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from "@angular/router/testing";
-import {By} from "@angular/platform-browser";
 import {Location} from '@angular/common';
 import {DOMHelper} from "../../../../../testing/dom-helper";
+import {of} from "rxjs";
+import {Item} from "../../../../shared/models/item.model";
 
 describe('ItemsListComponent', () => {
   let component: ItemsListComponent;
   let fixture: ComponentFixture<ItemsListComponent>;
   let dh: DOMHelper<ItemsListComponent>;
+  let itemServiceMock: any;
+  let helper: itemHelper;
 
   beforeEach(async(() => {
+    itemServiceMock = jasmine.createSpyObj('ItemService', ['getItemList']);
+    itemServiceMock.getItemList.and.returnValue(of([]));
     TestBed.configureTestingModule({
       declarations: [ItemsListComponent, testAddComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -22,6 +28,9 @@ describe('ItemsListComponent', () => {
           {path: 'add', component: testAddComponent}
         ]),
         HttpClientTestingModule
+      ],
+      providers: [
+        {provide: ItemService, useValue: itemServiceMock}
       ]
     })
       .compileComponents();
@@ -31,47 +40,54 @@ describe('ItemsListComponent', () => {
     fixture = TestBed.createComponent(ItemsListComponent);
     component = fixture.componentInstance;
     dh = new DOMHelper(fixture);
+    helper = new itemHelper();
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+
   it('should contain an h2 tag', () => {
     expect(dh.singleText('h2')).toBe('Items');
   });
+
 
   it('should be at "/" route of module', () => {
     const location = TestBed.get(Location);
     expect(location.path()).toBe('');
   });
 
+
   // add "async" to prevent "SPEC HAS NO EXPECTATIONS" alert
   it ('should redirect to "/add" on button click', async(() => {
     const location = TestBed.get(Location);
-    const buttons = fixture.debugElement.queryAll(By.css('button'));
-    const addButton:HTMLButtonElement = buttons[0].nativeElement;
+    dh.clickButton('Add New item');
 
-    addButton.click();
     fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect(location.path()).toBe('/add');
     });
   }));
 
+
   it ('should show "No Items", when itemsList empty', () => {
-    const noItems = fixture.debugElement.queryAll(By.css('.no-items'));
-    expect(noItems.length).toBe(1);
+    expect(dh.count('.no-items')).toBe(1);
   });
 
+
   it ('should show only 1 item in list', () => {
-    component.itemsList = [
-      { _id: '', title: 'test', photo: '/test/test', type: ['text'] }
-    ];
+    component.itemsList = helper.getItems(1);
     fixture.detectChanges();
-    const listItem = fixture.debugElement.queryAll(By.css('.item-box'));
-    expect(listItem.length).toBe(1);
+    expect(dh.count('.item-box')).toBe(1);
+  });
+
+
+  it ('should call "getItemList" of the ItemService one time on ngOnInit', () => {
+    fixture.detectChanges();
+    expect(itemServiceMock.getItemList).toHaveBeenCalledTimes(1);
   });
 
 });
@@ -80,4 +96,17 @@ describe('ItemsListComponent', () => {
 // Dummy component for route testing
 @Component({template: ''})
 class testAddComponent {
+}
+
+// Create necessary amount of dummy items
+class itemHelper {
+  itemsList:Item[] = [];
+  getItems(amount: number):Item[] {
+    for (let i = 0; i < amount; i++) {
+      this.itemsList.push(
+        { _id: 'abc' + i, title: 'test', photo: '/upload/name' + i + '.jpg', type: ['text'] }
+      );
+    }
+    return this.itemsList;
+  }
 }
